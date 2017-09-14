9fba56f0156cd90f532c7552bfbb2c17d86a98f9
GaziTaufiqIslam <gzidev@gmail.com>
2017-09-14 20:40:49 +0530

    Contact page toggle added

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,20 @@ $(document).ready(function(){
 $('.modal').modal();
 
   //new code
+  $(".contact-2").hide();
+$('#toggle-form1').on('click', function(e){
+e.preventDefault();
+  $(".contact-1").hide();
+  $(".contact-2").show();
+
+  });
+$('#toggle-form2').on('click', function(e){
+e.preventDefault();
+  $(".contact-2").hide();
+  $(".contact-1").show();
+
+});
+
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
     var winH = $(window).height();
