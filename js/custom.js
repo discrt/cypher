5ed1a17c8495a7a67ed55a9d3eb39ca04f940bf8
GaziTaufiqIslam <gzidev@gmail.com>
2017-09-19 21:48:18 +0530

    Scroll reveal added

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,6 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
 
-$('.modal').modal();
-
-  //new code
-  $(".contact-2").hide();
-$('#toggle-form1').on('click', function(e){
-e.preventDefault();
-  $(".contact-1").hide();
-  $(".contact-2").show();
-
-  });
-$('#toggle-form2').on('click', function(e){
-e.preventDefault();
-  $(".contact-2").hide();
-  $(".contact-1").show();
-
-});
-
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
     var winH = $(window).height();
@@ -29,4 +12,40 @@ e.preventDefault();
       $("nav").removeClass("atop");
     }
   });
+
+$('.modal').modal();
+
+  //new code
+
+
+
+
+  window.sr = ScrollReveal({
+    distance: '20px',
+    scale: 0.95,
+  });
+  sr.reveal('header .container video', {delay: 300, reset: true});
+  sr.reveal('#collab .container', {delay: 300, reset: true});
+  sr.reveal('#collab .sponsors', {delay: 300, reset: true});
+  sr.reveal('#action .row', {delay: 300, reset: true});
+  sr.reveal('.blog-section .row', {delay: 300, reset: true});
+  sr.reveal('#contact-wrapper .address-card', {delay: 300, reset: true});
+  sr.reveal('#contact-wrapper .contact-1');
+
+
+
+  $(".contact-2").hide();
+  $('#toggle-form1').on('click', function(e){
+  e.preventDefault();
+  $(".contact-1").hide();
+  $(".contact-2").fadeIn();
+
+  });
+  $('#toggle-form2').on('click', function(e){
+  e.preventDefault();
+  $(".contact-2").hide();
+  $(".contact-1").fadeIn();
+
+  });
+
 });
