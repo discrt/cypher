228a03e84304f971f8c19ca78207d2a61e9aa353
GaziTaufiqIslam <gzidev@gmail.com>
2017-09-20 23:16:05 +0530

    scroll reveal detailed part done 3.0

--- js/custom.js
+++ js/custom.js
@@ -22,13 +22,31 @@ $('.modal').modal();
 
   window.sr = ScrollReveal({
     distance: '20px',
-    scale: 0.95,
+    scale: 1,
+    viewFactor: 0.5
   });
-  sr.reveal('header .container video', {delay: 300, reset: true});
-  sr.reveal('#collab .container .col', {delay: 300, reset: true});
-  sr.reveal('#collab .sponsors  .col', {delay: 300, reset: true});
-  sr.reveal('#action .row  .col', {delay: 300, reset: true});
-  sr.reveal('.blog-section .row ', {delay: 100});
+  sr.reveal('.navbar-fixed .container', {origin: 'top', distance: '50px'});
+
+  sr.reveal('.navbar-fixed .material-icons', {origin: 'left', distance: '50px', delay: 300});
+
+  sr.reveal('header .container video', {delay: 200,origin: 'left', distance: '50px', scale: 0.90});
+  sr.reveal('header .container .left', {origin: 'left', distance: '10vw',duration: 900, delay: 1200});
+  sr.reveal('header .container .right', {origin: 'right', distance: '10vw',duration: 900,  delay: 1200});
+  sr.reveal('header .container h1', {origin: 'bottom', distance: '6vh',  delay: 400});
+  sr.reveal('header .container p', {origin: 'bottom', distance: '5vh', delay: 700});
+  sr.reveal('header .container .calls', {origin: 'bottom', distance: '4vh', delay: 900});
+
+  sr.reveal('#collab .container .col', {delay: 100, reset: true}, 200);
+  sr.reveal('#collab .sponsors  .col', {delay: 100, reset: true}, 100);
+
+  sr.reveal('#action .img-left', {delay: 100, reset: true, origin: 'left'});
+  sr.reveal('#action .col h3', {delay: 300, reset: true});
+  sr.reveal('#action .col .call-to', {delay: 500, reset: true});
+  sr.reveal('#action .b-right', { delay: 800, duration: 900, reset: true, origin: 'right', distance: '15vw'});
+  sr.reveal('#action .img-right', {delay: 100, reset: true, origin: 'right'});
+  sr.reveal('#action .b-left', {delay: 800, duration: 900, reset: true, origin: 'left', distance: '15vw'});
+
+  sr.reveal('.blog-section .row', {delay: 200});
   sr.reveal('#contact-wrapper .address-card', {delay: 300, reset: true});
   sr.reveal('#contact-wrapper .contact-1');
 
